0ff29c447fa1dbfa6290393b431b69cb8b792e80
Zekang <zkcheng721@gmail.com>
2020-02-17 21:30:40 +0000

    format

--- 1.cpp
+++ 1.cpp
@@ -4,9 +4,9 @@
 int main()
 {
   int sum;
-	for(int i = 1; i < 1000; i++)
+  for(int i = 1; i < 1000; i++)
   {
-		if (i % 5 == 0 || i % 3 == 0)
+    if (i % 5 == 0 || i % 3 == 0)
     {
       sum += i;
     }
